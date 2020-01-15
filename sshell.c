0d788fd42cdea07321d995773d457327e28bd6d2
Sayed Sadat <sysadat@ucdavis.edu>
2020-01-15 13:12:00 -0800

    Improved upon shell, basic fork, exec, wait commands

--- sshell.c
+++ sshell.c
@@ -2,17 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 #define CMDLINE_MAX 512
+#define ARGUMENTS_MAX 16
+#define TOKEN_MAX 32
 
-int main(void)
+int main(int argc, char *argv[])
 {
         char cmd[CMDLINE_MAX];
+        pid_t pid;
+        int status;
 
         while (1) {
                 char *nl;
-                int retval;
+                // int retval;
 
+                char *nullTest[1] = {NULL};
                 /* Print prompt */
                 printf("sshell$ ");
                 fflush(stdout);
@@ -22,7 +28,7 @@ int main(void)
 
                 /* Print command line if stdin is not provided by terminal */
                 if (!isatty(STDIN_FILENO)) {
-                        printf("%s", cmd);
+                        printf("dog");
                         fflush(stdout);
                 }
 
@@ -38,9 +44,25 @@ int main(void)
                 }
 
                 /* Regular command */
-                retval = system(cmd);
-                fprintf(stdout, "Return status value for '%s': %d\n",
-                        cmd, retval);
+                // retval = system(cmd);
+                // fprintf(stdout, "Return status value for '%s': %d\n",
+                //         cmd, retval);
+                if (argc == 1) {
+                  pid = fork();
+                  if (pid == 0) {
+                    execvp(argv[1],nullTest);
+                    perror("execvp");
+                    exit(1);
+                  } else if (pid > 0) {
+                    waitpid(pid, &status, 0);
+                    printf("Child exited with status: %d\n", WEXITSTATUS(status));
+                  } else {
+                    perror("fork");
+                    exit(1);
+                  }
+                }
+                // retval = system(cmd);
+                fprintf(stderr, "+ completed '%s' [%d]\n", argv[1], status);
         }
 
         return EXIT_SUCCESS;
